263a89ed6cf2f4b2f0069470c4f1196ede97dac4
Tharusha Thilakshan <tharushajayasekara04@gmail.com>
2024-03-12 11:29:25 +0530

    Inky Pinky

---
+++ src/Inky.java
@@ -0,0 +1,11 @@
+public class Inky {
+    public Inky(String Z) {
+        System.out.println(Z);
+    }
+
+    public Inky(String X, String Y, String V) {
+        System.out.println(Y + " " + X);
+        System.out.println(X + " " + V);
+        System.out.println(Y + " " + X + " " + V);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String pinky = "donky";
+
+        Inky ponky = new Inky(pinky);
+
+        String blinky = "pinky";
+        String rinky = "monky";
+
+        Inky ponky02 = new Inky(pinky, blinky, rinky);
+        }
+    }
